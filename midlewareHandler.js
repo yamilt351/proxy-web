0acc6b5a6c9b03e9059a3f9bb86bf4b443442b28
yamil351 <leokasper@live.com.ar>
2023-05-06 09:06:08 -0300

    creating midleware handler

---
+++ midlewareHandler.js
@@ -0,0 +1,19 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const mdilewareDir = path.join(__dirname, 'middlewares');
+
+const importMidlewares = async () => {
+  const middlewareFiles = await fs.promises.readdir(mdilewareDir);
+  const midlewareArr = [];
+  for (const file of middlewareFiles) {
+    const middleware = await import(`./src/midlewares/${file}`);
+    midlewareArr.push(middleware.default);
+    console.log(midlewareArr);
+  }
+  return midlewareArr;
+};
+
+export default importMidlewares;
