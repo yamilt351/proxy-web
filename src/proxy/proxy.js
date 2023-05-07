c7729079a92c3632a1aa05a83ae7aa253b4becc6
yamil351 <leokasper@live.com.ar>
2023-05-07 16:34:32 -0300

    checkpoint

--- src/proxy/proxy.js
+++ src/proxy/proxy.js
@@ -1,8 +1,5 @@
 import { createProxyMiddleware } from 'http-proxy-middleware';
-
-
-
-
+import { parse } from 'url';
 
 function redirect(req, res, next) {
   res.redirect(
@@ -24,14 +21,50 @@ function queryCheck(req, res, next) {
 function urlCreation(req, res, next) {
   queryCheck(req, res, next);
 
-  // split the url to proxy
-  const url = (req.url || '').replace('/?url=', '');
+  // Parse the request URL
+  const parsedUrl = parse(req.url, true);
+  const url = 'https://www.lanacion.com.ar/';
+  // Check if it's an asset request
+  if (parsedUrl.pathname.startsWith('/assets/')) {
+    // Return the asset URL, replacing '/assets/' with the desired base URL
+    return url + parsedUrl.pathname.replace('/assets/', '/');
+  }
 
-  return url;
+  // Check if the query contains the desired parameter (e.g., 'url')
+  if (parsedUrl.query && parsedUrl.query.url) {
+    // Create a new URL object from the target URL
+    const targetUrl = new URL(parsedUrl.query.url);
+
+    // Update target URL's pathname and search with the original request's values
+    targetUrl.pathname = parsedUrl.pathname;
+    targetUrl.search = parsedUrl.search;
+
+    // Return the formatted target URL
+    if (
+      parsedUrl.pathname === '/pf/resources/packages/css/homeln10-style.min.css'
+    ) {
+      targetUrl.search = '?d=1196';
+    } else {
+      targetUrl.search = parsedUrl.search;
+    }
+
+    return targetUrl.toString();
+  } else {
+    // Handle the case when the 'url' parameter is not present
+    console.error('URL parameter is missing');
+    return;
+  }
 }
 
 function proxyCreation(req, res, next) {
   const url = urlCreation(req, res, next);
+
+  if (!url) {
+    // Handle the invalid URL case, e.g., return an error response
+    res.status(400).send('Invalid URL');
+    return;
+  }
+
   // create proxy
   const proxy = createProxyMiddleware({
     target: url,
